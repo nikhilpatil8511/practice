8979f1017da085bc0f61b01ae4ff1dd334580923
nikhilpatil8511 <35782341+nikhilpatil8511@users.noreply.github.com>
2018-01-25 15:24:01 -0500

    Add files via upload

---
+++ Test_Proj/device_init.h
@@ -0,0 +1,12 @@
+#include "dr7f701057.dvf.h"
+#include "intVecNumF1L.h"
+
+#define RET_RAM_BEG   0xFEE00000
+#define RET_RAM_END   0xFEE07FFF
+
+#define protected_write(preg,pstatus,reg,value)   do{\
+                                                  (preg)=0xa5u;\
+                                                  (reg)=(value);\
+                                                  (reg)=~(value);\
+                                                  (reg)=(value);\
+                                                  }while((pstatus)==1u)
---
+++ Test_Proj/main.c
@@ -0,0 +1,16 @@
+/***********************************************************************/
+/*                                                                     */
+/*  FILE        :Main.c                                                */
+/*  DATE        :Mon, Jan 25, 2018                                     */
+/*  DESCRIPTION :This program is for testing basic fuctions of MC      */
+/*  CPU TYPE    :TH850                                                 */
+/*                                                                     */
+/*                                                                     */
+/***********************************************************************/
+
+void main(void);
+
+void main(void)
+{
+
+}
